37ad0605db6354d1e9926b84bfd2618e65f3741c
Lev Gryzhenkov <levgryzhenkov2002@gmail.com>
2022-11-25 03:04:04 +0200

    added picture generating function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,42 @@ import './App.scss';
 import TextInput from "../Input";
 import Loader from "../Loader";
 import Button from "../Button";
+import {Configuration, OpenAIApi} from "openai";
+import {useState} from "react";
+
+const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_CONFIGURATION_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const App = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const generateImage = async () => {
+        try {
+            setLoading(true);
+            const imageParameters = {
+                prompt: inputValue,
+                n: 1,
+                size: "512x512",
+            }
+            const response = await openai.createImage(imageParameters);
+            const urlData = response.data.data[0].url;
+            setImageUrl(urlData);
+            setLoading(false)
+        } catch (e) {
+            setLoading(false)
+        }
+    }
+
+    const onInputChange = (e) => setInputValue(e.target.value);
+    const onButtonClick = () => {
+        generateImage();
+        setInputValue('')
+    }
+
     return (
         <div className="App">
             <div className="main">
@@ -13,19 +47,21 @@ const App = () => {
                 </p>
                 <div className="flex">
                     <div className="logo-container">
-                        <img src={logo} className="App-logo" alt="logo"/>
+                        {loading
+                            ? <Loader/>
+                            : <img src={imageUrl ? imageUrl : logo} className="App-logo" alt="ai picture"/>}
+
                     </div>
                 </div>
                 <p className="description">Simply enter the description you need,
                     <br/>
-                    and generator will take care of the rest.</p>
+                    and generator will take care of the rest ;)</p>
                 <div className="input-container">
-                    <TextInput placeholder="Write a description here :)"/>
+                    <TextInput onChange={onInputChange} value={inputValue} placeholder="Write a description here"/>
                     <div className="button-container">
-                        <Button>Generate</Button>
+                        <Button onClick={onButtonClick} disabled={!inputValue}>Generate</Button>
                     </div>
                 </div>
-                {/*<Loader/>*/}
             </div>
         </div>
     );
